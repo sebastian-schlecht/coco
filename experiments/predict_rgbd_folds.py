6bcaf2b4bab146b8696fc10e3f079e7d0881401f
Sebastian <mail@sebastian-schlecht.de>
2017-01-10 04:29:20 +0100

    Evaluation notebook

--- experiments/predict_rgbd_folds.py
+++ experiments/predict_rgbd_folds.py
@@ -1,3 +1,6 @@
+"""
+In order to compare inferred and GT depth together with RGB input for BU regression, we'd like to make the comparison as fair as possible. Thus, we compile a dataset for the inferred case where the depth is predicted by a network that has never seen that particular sample before during its training phase. We thus compile a dataset with inferred depth based on the learned folds.
+"""
 import sys
 import os
 import inspect
@@ -14,6 +17,8 @@ from coco.multiprocess import MultiProcessor
 from coco.architectures.depth import DepthPredictionScaffolder, ResidualDepth
 from coco.transformations import zoom_rotate, random_rgb, random_crop, normalize_images, downsample, clip, noise, exp, flip_x
 from coco.job import Job
+
+from scipy.ndimage.interpolation import zoom
 
 global mean
 mean = np.load("/ssd/food3d/f3d-train.npy")
@@ -51,7 +56,7 @@ def main():
     ds_labels = []
     
     for train, val, model in db_models:
-        print "Predicting depth for db: %s" % db
+        print "Predicting depth for db: %s" % train
         scaffolder = DepthPredictionScaffolder(ResidualDepth, inference=True, k=0.5)
         scaffolder.compile()
         scaffolder.load(model)
@@ -61,7 +66,7 @@ def main():
         
         for index in range(ilen):
             image = np.array(f["rgbd"][index])[np.newaxis,:,:,:]
-            # Just copy, separation si done by processing function
+            # Just copy, separation is done by processing function
             label = image.copy()
             
             # Preprocess
@@ -76,7 +81,14 @@ def main():
             prediction = prediction.clip(lower, upper)
             
             # Up-sample
+            prediction = predicition.squeeze()
+            f_h = 240. / prediction.shape[0]
+            f_w = 320. / prediction.shape[1]
+            prediction = zoom(prediction, (f_h, f_w))
+            # Add num and channel axis again
+            prediction = prediction[np.newaxis, np.newaxis, :, :]
             
+            # Store
             ds_images.append(image)
             ds_depths.append(prediction)
             ds_bus.append(f["bus"][index])
@@ -87,6 +99,7 @@ def main():
     # We want to provide similar conditions to both nets so we iterate the train dbs and find all corresponding dishes
     # The result will contain every image twice but that's ok as they would be re-iterated anyways after the next epoch
     for (train, val, model) in db_models:
+        print "Building fold for db: %s" % train
         f = h5py.File(train)
         labels = np.array(f["labels"])
         new_train_db = train.replace("train", "infer-train")
@@ -94,15 +107,38 @@ def main():
         
         val_rgbd = []
         val_labels = []
+        val_bus = []
+        
         train_rgbd = []
         train_labels = []
+        train_bus = []
         
         # Fetch images from labels in the train set
         for idx in range(len(ds_labels)):
             rgbd = np.concatenate([ds_images[idx],ds_depths[idx]], axis=1)
             if ds_labels[idx] in labels:
-                train_rgbd.apppend()
-            
+                train_rgbd.apppend(rgbd)
+                train_labels.append(ds_labels[idx])
+                train_bus.append(ds_bus[idx])
+            else:
+                val_rgbd.apppend(rgbd)
+                val_labels.append(ds_labels[idx])
+                val_bus.append(ds_bus[idx])
+                
+        # Save
+        train_db = h5py.File(new_train_db + ".new")
+        train_db.create_dataset("rgbd", data=np.concatenate(train_rgbd))
+        train_db.create_dataset("bus", data=np.array(train_bus))
+        train_db.create_dataset("labels", data=np.array(train_labels))
+        
+        val_db = h5py.File(new_val_db + ".new")
+        val_db.create_dataset("rgbd", data=np.concatenate(val_rgbd))
+        val_db.create_dataset("bus", data=np.array(val_bus))
+        val_db.create_dataset("labels", data=np.array(val_labels))
+        
+        train_db.close()
+        val_db.close()
+        
                           
                         
                           
